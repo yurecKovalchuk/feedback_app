05ec40f602735413911b20fdee837d60a4fee9c7
yurecKovalchuk <KovalchukYurec@hotmail.com>
2023-11-20 18:28:24 +0200

    feat: add logger for application

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -3,15 +3,30 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
+import 'package:logging/logging.dart';
+
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async =>
     runZonedGuarded(() async => _initialize(builder), _handleError);
 
 void _initialize(FutureOr<Widget> Function() builder) async {
+  _initLogger();
+
   return runApp(await builder());
 }
 
+void _initLogger() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((record) {
+    if (kDebugMode) {
+      print('${record.level.name}: ${record.time}: ${record.message}');
+    }
+  });
+}
+
 void _handleError(Object error, StackTrace stack) {
+  final logger = Logger('_handleError');
+
   if (kDebugMode) {
-    print(error);
+    logger.warning('Error: $error; stack: $stack');
   }
 }
