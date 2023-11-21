abfcf50f6fa70b2cd7afffc6fbfefd93270d3746
yurecKovalchuk <KovalchukYurec@hotmail.com>
2023-11-21 08:09:29 +0200

    refactor: reformat code

--- lib/exceptions/validation_exception.dart
+++
@@ -1,9 +0,0 @@
-// import '../domain/models/models.dart';
-//
-// class ValidationException implements Exception {
-//   ValidationException(
-//       this.response,
-//       );
-//
-//   final ErrorResponse response;
-// }
