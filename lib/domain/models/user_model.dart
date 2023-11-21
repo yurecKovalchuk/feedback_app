f54375ca9b934aaccde88d5dd1386f534f939bc9
yurecKovalchuk <KovalchukYurec@hotmail.com>
2023-11-21 04:07:37 +0200

    feat: add user model

---
+++ lib/domain/models/user_model.dart
@@ -0,0 +1,11 @@
+class UserModel {
+  UserModel({
+    required this.name,
+    required this.email,
+    required this.message,
+  });
+
+  final String name;
+  final String email;
+  final String message;
+}
